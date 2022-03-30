0914fe9754ad4373211eeb6462a3f7d818cc872b
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-03-30 00:22:52 +0000

    First commit

---
+++ src/lib/svelte-cusdis/index.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { onMount } from "svelte";
+     export let attrs;
+    export let lang = undefined;
+
+    onMount(async ()=>{
+
+
+        if (lang){
+            let langScript = document.querySelector(`script[src="${lang}"]`);
+            if (!langScript){
+                langScript = document.createElement("script");
+                langScript.setAttribute("defer",true);
+                langScript.setAttribute("async",true);
+                document.querySelector("head").appendChild(langScript);
+            }
+        }
+
+        let url = "https://cusdis.com/js/cusdis.es.js";
+        let script  = document.querySelector(`script[src="${url}"]`);
+        if (!script){
+            script = document.createElement("script");
+            script.src = url;
+            script.defer = true;
+            script.async = true;
+            document.querySelector("head").appendChild(script);
+        }
+    })
+</script>
+
+<div
+id = "cusdis_thread"
+data-host = {attrs.host || "https://cusdis.com"}
+data-app-id = {attrs.appId}
+data-page-id = {attrs.pageId}
+data-page-url = {attrs.Url}
+data-page-title = {attrs.pageTitle} 
+data-theme = {attrs.theme}
+/>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Cusdis from "$lib/svelte-cusdis";
+</script>
+
+<svelte:head>
+	<title>Svelte Cusdis</title>
+</svelte:head>
+
+<p style:text-align="center">
+	Cusdis integration for Svelte
+</p>
+
+
+<Cusdis
+ attrs = {{
+	 appId: "2f49c941-a723-4350-a9eb-cad6fab4772b",
+	 pageId: "1",
+	 pageUrl: "https:localhost:3000",
+	 pageTitle: "Svelte Cusdis"
+ }}
+/>
